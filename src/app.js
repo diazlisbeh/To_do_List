cea1afd4844786e65d5c179e88013ad95d9039ff
Lisbeth Diaz <lizbeths.robles@gmail.com>
2022-02-21 12:34:52 -0400

    added remove function

--- src/app.js
+++ src/app.js
@@ -1,13 +1,31 @@
+//constats declaration
 const addTaskButton = document.getElementById("addTaskButton")
-const deleteTaskButton = document.getElementById("deleteTaskButton")
 const seccion = document.querySelector(".seccion")
 const task = document.querySelector(".task")
+const titleTask = document.querySelector(".task-container__Title")
+
+//Variables for the nodeList of deletes bottons
+let deleteTaskButton = document.querySelectorAll(".task-delete")
+let lastNode = deleteTaskButton[deleteTaskButton.length - 1]
+
 
 addTaskButton.addEventListener("click", addTask = ()=>{
   
   let node = document.createElement("div")
   node.appendChild(task.cloneNode(task))
-//node.children[0].children[0].children[1].value = ""
+  node.children[0].children[0].children[1].textContent = "Title of the task"
+  node.children[0].children[0].children[0].checked = false
+  
   seccion.appendChild(node)
-//console.log(node.children[0].children[0].children[1].placeholder)
+  
+  //Adding event listening for each delete button
+  deleteTaskButton = document.querySelectorAll(".task-delete")
+  lastNode = deleteTaskButton[deleteTaskButton.length - 1]
+  lastNode.addEventListener("click", deleteTask)
+
 })
+
+function deleteTask(e){
+  e.path[3].remove()
+}
+
