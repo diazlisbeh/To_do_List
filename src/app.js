ee03475e91a516fe1912d4e2273082232ca478ff
Lisbeth Diaz <lizbeths.robles@gmail.com>
2022-02-22 21:33:01 -0400

    Add Function for details button

--- src/app.js
+++ src/app.js
@@ -8,6 +8,12 @@ const titleTask = document.querySelector(".task-container__Title")
 let deleteTaskButton = document.querySelectorAll(".task-delete")
 let lastNode = deleteTaskButton[deleteTaskButton.length - 1]
 
+//Details Container and button
+let detailsContainer = document.querySelectorAll("#details-container")
+let lastDetailsNode = detailsContainer[detailsContainer.length - 1]
+
+let taskDetailsButton = document.querySelectorAll(".task-details__button")
+let lastDetailsButtonNode = detailsContainer[detailsContainer.length - 1]
 
 addTaskButton.addEventListener("click", addTask = ()=>{
   
@@ -23,9 +29,28 @@ addTaskButton.addEventListener("click", addTask = ()=>{
   lastNode = deleteTaskButton[deleteTaskButton.length - 1]
   lastNode.addEventListener("click", deleteTask)
 
+  //Adding envent listening for each details button
+  taskDetailsButton = document.querySelectorAll(".task-details__button")
+  lastDetailsButtonNode = taskDetailsButton[taskDetailsButton.length - 1]
+  lastDetailsButtonNode.addEventListener("click", showDetails)
+
+
+  detailsContainer = document.querySelectorAll("#details-container")
+  lastDetailsNode = detailsContainer[detailsContainer.length - 1]
+
+
 })
 
 function deleteTask(e){
   e.path[3].remove()
 }
+
+
+
+function showDetails (e){
+  let detailsContainer = e.path[3].children[2]
+  let displayF = detailsContainer.classList.toggle("display")
+
+}
+
 
